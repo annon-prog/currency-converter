2a562608ad01d867304e14b3a3c2b721e837b402
annon-prog <fidelisdela.wambui@gmail.com>
2023-11-08 22:00:00 +0300

    finished on the Javascript logic

--- script.js
+++ script.js
@@ -40,5 +40,110 @@ function shillingToPound() {
   }
 }
 
+function dollarToShilling() {
+  let secondDollar = firstSelectedElement.value;
+  let thirdShilling = secondSelectedElement.value;
+  let thirdAmountStored = Number(amount.value);
+  let thirdConvertedValue = thirdAmountStored * 151.51429;
+  if (secondDollar === "dollars" && thirdShilling === "shillings") {
+    result.innerHTML += `
+  <div>
+  <p> ${thirdAmountStored}.00 US Dollars = ${thirdConvertedValue} Kenyan Shillings
+  </div>
+  `;
+  }
+}
+
+function dollarToPound() {
+  let thirdDollar = firstSelectedElement.value;
+  let secondPound = secondSelectedElement.value;
+  let fourthAmountStored = Number(amount.value);
+  let fourthConvertedValue = fourthAmountStored * 0.81358576;
+  if (thirdDollar === "dollars" && secondPound === "pounds") {
+    result.innerHTML += `
+  <div>
+  <p> ${fourthAmountStored}.00 US Dollars = ${fourthConvertedValue} British Pounds
+  </div>
+  `;
+  }
+}
+
+function poundToDollar() {
+  let thirdPound = firstSelectedElement.value;
+  let fourthDollar = secondSelectedElement.value;
+  let fifthAmountStored = Number(amount.value);
+  let fifthConvertedValue = fifthAmountStored * 1.2292301;
+  if (thirdPound === "pounds" && fourthDollar === "dollars") {
+    result.innerHTML += `
+  <div>
+  <p> ${fifthAmountStored}.00 British Pounds = ${fifthConvertedValue} US Dollars
+  </div>
+  `;
+  }
+}
+
+function poundToShilling() {
+  let fourthPound = firstSelectedElement.value;
+  let fourthShilling = secondSelectedElement.value;
+  let sixthAmountStored = Number(amount.value);
+  let sixthConvertedValue = sixthAmountStored * 186.22116;
+  if (fourthPound === "pounds" && fourthShilling === "shillings") {
+    result.innerHTML += `
+  <div>
+  <p> ${sixthAmountStored}.00 British Pounds = ${sixthConvertedValue} Kenyan Shillings
+  </div>
+  `;
+  }
+}
+
+function shillingToShilling() {
+  let fifthShilling = firstSelectedElement.value;
+  let sixthShilling = secondSelectedElement.value;
+  let seventhAmountStored = Number(amount.value);
+  let seventhConvertedValue = seventhAmountStored * 1;
+  if (fifthShilling === "shillings" && sixthShilling === "shillings") {
+    result.innerHTML += `
+  <div>
+  <p> ${seventhAmountStored}.00 Kenyan Shillings = ${seventhConvertedValue}.00 Kenyan Shillings
+  </div>
+  `;
+  }
+}
+
+function dollarToDollar() {
+  let fifthDollar = firstSelectedElement.value;
+  let sixthDollar = secondSelectedElement.value;
+  let eighthAmountStored = Number(amount.value);
+  let eighthConvertedValue = eighthAmountStored * 1;
+  if (fifthDollar === "dollars" && sixthDollar === "dollars") {
+    result.innerHTML += `
+  <div>
+  <p> ${eighthAmountStored}.00 US Dollars = ${eighthConvertedValue}.00 US Dollars
+  </div>
+  `;
+  }
+}
+
+function poundToPound() {
+  let fifthPound = firstSelectedElement.value;
+  let sixthPound = secondSelectedElement.value;
+  let ninthAmountStored = Number(amount.value);
+  let ninthConvertedValue = ninthAmountStored * 1;
+  if (fifthPound === "pounds" && sixthPound === "pounds") {
+    result.innerHTML += `
+  <div>
+  <p> ${ninthAmountStored}.00 British Pounds = ${ninthConvertedValue}.00 British Pounds
+  </div>
+  `;
+  }
+}
+
 button.addEventListener("click", shillingToDollar);
 button.addEventListener("click", shillingToPound);
+button.addEventListener("click", dollarToShilling);
+button.addEventListener("click", dollarToPound);
+button.addEventListener("click", poundToDollar);
+button.addEventListener("click", poundToShilling);
+button.addEventListener("click", shillingToShilling);
+button.addEventListener("click", dollarToDollar);
+button.addEventListener("click", poundToPound);
